0f8869878f7013daf384c4af496933e6058e4109
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2025-01-15 05:42:49 +0100

    create pbr frag and vert shaders

--- lettier/src/main.c
+++ lettier/src/main.c
@@ -6,6 +6,51 @@
 #define GLSL_VERSION            100
 #endif
 
+#define MAX_LIGHTS 4
+
+//------------------------------------------------------------------------------------
+// Types and Structures Definition
+//------------------------------------------------------------------------------------
+// light type
+typedef enum {
+    LIGHT_DIRECTIONAL = 0,
+    LIGHT_POINT,
+    LIGHT_SPOT
+} LightType;
+
+// Light data
+typedef struct {
+    int type;
+    int enabled;
+    Vector3 position;
+    Vector3 target;
+    float color[4];
+    float intensity;
+
+    // Shader light parameters locations
+    int typeLoc;
+    int enabledLoc;
+    int positionLoc;
+    int targetLoc;
+    int colorLoc;
+    int intensityLoc;
+} Light;
+
+//----------------------------------------------------------------------------------
+// Global Variables Definition
+//----------------------------------------------------------------------------------
+static int lightCount = 0;     // Current number of dynamic lights that have been created
+
+//----------------------------------------------------------------------------------
+// Module specific Functions Declaration
+//----------------------------------------------------------------------------------
+// Create a light and get shader locations
+static Light CreateLight(int type, Vector3 position, Vector3 target, Color color, float intensity, Shader shader);
+
+// Update light properties on shader
+// NOTE: Light shader locations should be available
+static void UpdateLight(Shader shader, Light light);
+
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
@@ -27,14 +72,10 @@ int main(void)
     camera.fovy = 45.0f;                                // Camera field-of-view Y
     camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
 
+    Shader shader = LoadShader(TextFormat("resources/shaders/pbr.vert", GLSL_VERSION), TextFormat("resources/shaders/pbr.frag", GLSL_VERSION));
+
+    // Raylib loads and unloads textures automatically for .gltf / .glb
     Model model = LoadModel("resources/scene.gltf");
-//    Texture texture = LoadTexture("resources/torus_diffuse.png");
-//    // Load shader for model
-//    // NOTE: Defining 0 (NULL) for vertex shader forces usage of internal default vertex shader
-//    Shader shader = LoadShader(0, TextFormat("resources/shaders/grayscale.frag", GLSL_VERSION));
-//
-//    model.materials[1].shader = shader;                     // Set shader effect to 3d model
-//    model.materials[1].maps[MATERIAL_MAP_DIFFUSE].texture = texture; // Bind texture to model
 
     Vector3 position = { 0.0f, 0.0f, 0.0f };    // Set model position
 
@@ -74,8 +115,6 @@ int main(void)
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-//    UnloadShader(shader);   // Unload shader
-//    UnloadTexture(texture);     // Unload texture
     UnloadModel(model);         // Unload model
 
     CloseWindow();          // Close window and OpenGL context
